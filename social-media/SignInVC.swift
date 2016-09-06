5b9c39ca02209835ca2099f98154a22fd2066609
Peter Rule <peterrule27@gmail.com>
2016-09-06 14:46:00 +0100

    Firebase integrated

---
+++ social-media/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  social-media
+//
+//  Created by Peter Rule on 06/09/2016.
+//  Copyright © 2016 Peter Rule. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
