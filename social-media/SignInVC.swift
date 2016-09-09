0a13ef385ff267d730d6f298b892f515ef3ba754
Peter Rule <peterrule27@gmail.com>
2016-09-09 13:14:48 +0100

    finished but not authenticating

--- social-media/SignInVC.swift
+++ social-media/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,32 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func facebookBtnTapped(_ sender: AnyObject) {
+        
+        let faceBookLogin = FBSDKLoginManager()
+        faceBookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("PETE: Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("PETE: User cancelled Facebook authentification")
+                
+            } else {
+                print("PETE: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.fireBaseAuth(credential)
+            }
+        }
+    }
+    
+    func fireBaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("PETE: unable to authenticate with Firebase - \(error)")
+            } else {
+                print("PETE: Successfully authenticated with Firbase")
+            }
+        })
+    }
 }
+
 
